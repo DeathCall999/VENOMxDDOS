1eb53cc38f01e813d1199e880bd6fdb7f6f46d51
DeathCall999 <lumbemongullie07@gmail.com>
2024-06-29 18:05:53 +0530

    Update m.py

--- m.py
+++ m.py
@@ -8,10 +8,10 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('TOKEN')
+bot = telebot.TeleBot('7455416030:AAE1UiMU8kGN3SMjlSLrepMmxaXrueLvzpg')
 
 # Admin user IDs
-admin_id = ["your id"]
+admin_id = ["6477266175"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
@@ -62,7 +62,7 @@ def log_command(user_id, target, port, time):
 def clear_logs():
     try:
         with open(LOG_FILE, "r+") as file:
-            if file.read() == "":
+             if file.read() == "":
                 response = "Logs are already cleared. No data found ❌."
             else:
                 file.truncate(0)
